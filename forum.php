a425529d777924dbd65fedc9c84b3ad6013661c6
HitsuKuro <32584137+HitsuKuro@users.noreply.github.com>
2019-03-06 15:48:56 +0100

    Update forum.php

--- forum.php
+++ forum.php
@@ -12,10 +12,10 @@
         <nav>  
             <div class="menu">
                 <ul>
-                    <a href="index.html" target="_blank">HOME</a>
+                    <a class="navthing" href="index.html" target="_blank">HOME</a>
                     <a class="active" href="#">FORUM</a>
-                    <a href="staff.html" target="_blank">STAFF List</a>
-                    <a href="#support" target="_blank">SUPPORT US</a>
+                    <a class="navthing" href="staff.html" target="_blank">STAFF List</a>
+                    <a class="navthing" href="index.html#support" target="_blank">SUPPORT US</a>
                 </ul>
             </div>
         </nav>
