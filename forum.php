53a328f29adfde2f931087fe7a6240ea265ad37e
HitsuKuro <32584137+HitsuKuro@users.noreply.github.com>
2019-03-06 15:46:06 +0100

    Create forum.php

---
+++ forum.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Deviants Network</title>
+        <link rel="stylesheet" href="stylesheet.css">
+        
+    </head>
+    <body>
+        <header>
+                <h1> Deviants Network </h1>
+        </header>
+        <nav>  
+            <div class="menu">
+                <ul>
+                    <a href="index.html" target="_blank">HOME</a>
+                    <a class="active" href="#">FORUM</a>
+                    <a href="staff.html" target="_blank">STAFF List</a>
+                    <a href="#support" target="_blank">SUPPORT US</a>
+                </ul>
+            </div>
+        </nav>
+    </body>
+</html>
